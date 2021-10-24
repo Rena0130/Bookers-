f53c0e1492279538bb768f0ab4e741c083e1adf1
EC2 Default User <ec2-user@ip-172-31-21-43.ec2.internal>
2021-10-24 20:36:48 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  
+  resources :users, only: [:index, :show, :edit, :update] do
+    get :following
+    get :followed
+  end
+  
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  
+   get 'relationships/create'
+   get 'relationships/destroy'
+   post 'follow/:id' => 'relationships#follow', as: 'follow'
+   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+   get '/search' => 'search#search'
 end
